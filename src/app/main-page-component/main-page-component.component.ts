7b54d3764f587e36a6c7dc54d838dad51ba8fe18
GazingStars <ra.md@list.ru>
2025-02-15 22:41:40 +0300

    init: initial commit

---
+++ src/app/main-page-component/main-page-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page-component',
+  imports: [],
+  templateUrl: './main-page-component.component.html',
+  styleUrl: './main-page-component.component.scss'
+})
+export class MainPageComponentComponent {
+
+}

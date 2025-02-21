d0856771e5a186745653b192f4373e2041ce9969
GazingStars <ra.md@list.ru>
2025-02-21 16:26:55 +0300

    feat: add main screen for online cinema with styling
    There are actually 2 page designs (second one is commented), I like this one less but others liked it more.
    If you want to see the second one you should uncomment div class="container" and height shold be 70vh

--- src/app/main-page-component/main-page-component.component.ts
+++ src/app/main-page-component/main-page-component.component.ts
@@ -1,36 +1,82 @@
-import { Component } from '@angular/core';
-import { CurrencyPipe } from '@angular/common';
+import { Component, OnInit, OnDestroy  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-main-page-component',
-  imports: [CurrencyPipe],
+  imports: [CommonModule],
   templateUrl: './main-page-component.component.html',
   styleUrl: './main-page-component.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      state('visible', style({ opacity: 1 })),
+      state('hidden', style({ opacity: 0 })),
+      transition('hidden => visible', [
+        animate('1s ease-in-out')
+      ]),
+      transition('visible => hidden', [
+        animate('1s ease-in-out')
+      ])
+    ])
+  ]
 })
-export class MainPageComponentComponent {
-  isVisible = false;
-  fruts = [
-    {
-      id: 0,
-      name: 'Огурец',
-      desc: 'Красивый и зеленый',
-      price: 50,
-    },
-    {
-      id: 1,
-      name: 'Помидор',
-      desc: 'Круглый и красный',
-      price: 30,
-    },
-    {
-      id: 2,
-      name: 'Яблоко',
-      desc: 'Круглое и ... тоже красное',
-      price: 40,
-    },
-  ];
 
-  toggleVisibility() {
-    this.isVisible = !this.isVisible;
+export class MainPageComponentComponent implements OnInit, OnDestroy {
+  movies: any[] = []; 
+  randomNumber = 0;
+  rPoster: any = '';
+  private intervalId: any; 
+  animationState = 'visible';
+  
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getTopMovies();
   }
+
+  getTopMovies(): void {
+    this.http
+      .get<any>(
+        'https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_250_MOVIES&page=1',
+        {
+          headers: {
+            'X-API-KEY': '63d28fb1-f0f3-490e-ad75-8cfe6a51aabc',
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      .subscribe({
+        next: (data) => {
+          this.movies = data.items;
+          console.log(this.movies);
+          if (this.movies.length > 0) {
+            this.updatePoster(); 
+            this.startImageRotation(); 
+          }
+        },
+      });
+  }
+
+  updatePoster(): void {
+    this.animationState = 'hidden';
+    setTimeout(() => {
+      const randomIndex = Math.floor(Math.random() * this.movies.length);
+      this.rPoster = this.movies[randomIndex].coverUrl;
+      this.animationState = 'visible'; 
+    }, 1000);
+  }
+
+  startImageRotation(): void {
+    this.intervalId = setInterval(() => {
+      this.updatePoster();
+    }, 5000); 
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
 }
